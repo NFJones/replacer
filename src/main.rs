e9cb902b102abd6983a57b3610bf7efe6076da34
Neil Jones <neil.franklin.jones@gmail.com>
2021-02-07 19:35:32 -0600

    Use clap macro for args.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@
 */
 mod replacer;
 
-use clap::{App, Arg};
+use clap::Clap;
 use regex::Regex;
 use replacer::error::{set_debug, CliError};
 use replacer::scan_buffer::ScanBuffer;
@@ -32,30 +32,109 @@ use std::io::Read;
 use std::io::Write;
 use std::iter::FromIterator;
 
-#[derive(Debug, Clone)]
-struct Cli {
+#[clap(
+    name = "rp",
+    version = "0.1.0",
+    author = "Neil F Jones",
+    about = "A multiline regex find/replace utility."
+)]
+#[derive(Debug, Clone, Clap)]
+struct Opts {
+    #[clap(
+        short('i'),
+        long("inplace"),
+        takes_value(false),
+        about("The regex pattern to match.")
+    )]
     inplace: bool,
-    pattern: Result<String, CliError>,
-    replacement: Result<String, CliError>,
+    #[clap(
+        short('p'),
+        long("pattern"),
+        takes_value(true),
+        validator(validate_regex),
+        conflicts_with("pattern-file"),
+        about("Write to file instead of stdout.")
+    )]
+    pattern: Option<String>,
+    #[clap(
+        short('r'),
+        long("replacement"),
+        takes_value(true),
+        validator(validate_regex),
+        conflicts_with("replacement-file"),
+        about("The replacement text to write. Supports groups (${1}, ${named_group}, etc.)")
+    )]
+    replacement: Option<String>,
+    #[clap(
+        short('P'),
+        long("pattern-file"),
+        takes_value(true),
+        validator(validate_regex_file),
+        conflicts_with("pattern"),
+        about("The file to read the regex pattern from.")
+    )]
+    pattern_file: Option<String>,
+    #[clap(
+        short('R'),
+        long("replacement-file"),
+        takes_value(true),
+        validator(validate_regex_file),
+        conflicts_with("replacement"),
+        about("The file to read the replacement text from.")
+    )]
+    replacement_file: Option<String>,
+    #[clap(
+        short('e'),
+        long("escape"),
+        takes_value(false),
+        about("Print the pattern with regex characters escaped.")
+    )]
     escape: bool,
-    pump_limit: i64,
+    #[clap(
+        short('l'),
+        long("pump-limit"),
+        takes_value(true),
+        validator(validate_pump_limit),
+        about("The internal buffer size when making streaming replacements.")
+    )]
+    pump_limit: Option<i64>,
+    #[clap(
+        short('v'),
+        long("verbose"),
+        takes_value(false),
+        about("Print verbose output to stderr.")
+    )]
     verbose: bool,
-    files: Option<Vec<String>>,
+    #[clap(multiple(true), about("Print verbose output to stderr."))]
+    files: Vec<String>,
 }
 
-fn regex_validator(val: &str) -> Result<String, CliError> {
+#[derive(Debug, Clone)]
+struct ParsedOpts {
+    pattern: String,
+    replacement: String,
+    pump_limit: i64,
+}
+
+#[derive(Debug, Clone)]
+struct Cli {
+    opts: Opts,
+    parsed_opts: ParsedOpts,
+}
+
+fn validate_regex(val: &str) -> Result<String, CliError> {
     match Regex::new(val) {
         Ok(_) => return Ok(String::from(val)),
         Err(error) => return Err(CliError::from(error)),
     }
 }
 
-fn regex_file_validator(val: &str) -> Result<String, CliError> {
+fn validate_regex_file(val: &str) -> Result<String, CliError> {
     let mut buffer = String::new();
     let file = File::open(val);
     match file {
         Ok(mut file) => match file.read_to_string(&mut buffer) {
-            Ok(_) => match regex_validator(buffer.as_str()) {
+            Ok(_) => match validate_regex(buffer.as_str()) {
                 Ok(_) => return Ok(String::from(val)),
                 Err(error) => return Err(CliError::from(error)),
             },
@@ -65,146 +144,58 @@ fn regex_file_validator(val: &str) -> Result<String, CliError> {
     }
 }
 
+fn validate_pump_limit(pump_limit: &str) -> Result<i64, CliError> {
+    match parse_size(pump_limit) {
+        Ok(pump_limit) => return Ok(pump_limit),
+        Err(error) => return Err(error),
+    }
+}
+
 impl Cli {
-    fn parse_args() -> clap::ArgMatches {
-        return App::new("rp")
-            .author("Neil Jones")
-            .about("A multiline regex find/replace utility.")
-            .version("0.1.0")
-            .args(&[
-                Arg::new("inplace")
-                    .short('i')
-                    .long("inplace")
-                    .takes_value(false)
-                    .about("Write to file instead of stdout."),
-                Arg::new("pattern")
-                    .short('p')
-                    .long("pattern")
-                    .takes_value(true)
-                    .validator(regex_validator)
-                    .about("The regex pattern to match."),
-                Arg::new("replacement")
-                    .short('r')
-                    .long("replacement")
-                    .takes_value(true)
-                    .about(
-                        "The replacement text to write. Supports groups (${1}, ${named_group}, etc.)",
-                    ),
-                Arg::new("pattern-file-path")
-                    .short('P')
-                    .long("pattern-file")
-                    .takes_value(true)
-                    .validator(regex_file_validator)
-                    .about("The file to read the regex pattern from."),
-                Arg::new("replacement-file-path")
-                    .short('R')
-                    .long("replacement-file")
-                    .takes_value(true)
-                    .about("The file to read the replacement text from."),
-                Arg::new("escape")
-                    .short('e')
-                    .long("escape-pattern")
-                    .takes_value(false)
-                    .about("Print the pattern with regex characters escaped."),
-                Arg::new("pump-limit")
-                    .short('l')
-                    .long("pump-limit")
-                    .takes_value(true)
-                    .default_value("1MiB")
-                    .about("The internal buffer size when making streaming replacements."),
-                Arg::new("verbose")
-                    .short('v')
-                    .long("verbose")
-                    .takes_value(false)
-                    .about("Print verbose output to stderr."),
-                Arg::new("files").multiple(true),
-            ])
-            .get_matches();
-    }
-
     fn new() -> Cli {
-        let args = Cli::parse_args();
-        let files = args.values_of("files");
-        let file_vec: Option<Vec<String>>;
-        match files {
-            Some(files) => {
-                file_vec = Some(
-                    files
-                        .map(|path| -> String { return String::from(path) })
-                        .collect(),
-                )
-            }
-            None => file_vec = None,
-        }
-
-        let cli = Cli {
-            inplace: args.is_present("inplace"),
-            pattern: Cli::get_arg_or_file(
-                "pattern",
-                args.value_of("pattern"),
-                args.value_of("pattern-file-path"),
-            ),
+        let opts = Opts::parse();
+        let parsed_opts = ParsedOpts {
+            pattern: Cli::get_arg_or_file(opts.pattern.clone(), opts.pattern_file.clone()),
             replacement: Cli::get_arg_or_file(
-                "replacement",
-                args.value_of("replacement"),
-                args.value_of("replacement-file-path"),
+                opts.replacement.clone(),
+                opts.replacement_file.clone(),
             ),
-            escape: args.is_present("escape"),
-            pump_limit: Cli::parse_pump_limit(args.value_of("pump-limit")),
-            verbose: args.is_present("verbose"),
-            files: file_vec,
+            pump_limit: opts.pump_limit.unwrap_or(1024 ^ 2),
         };
-        return cli;
+        return Cli { opts, parsed_opts };
     }
 
-    fn get_arg_or_file(
-        name: &str,
-        arg: Option<&str>,
-        path: Option<&str>,
-    ) -> Result<String, CliError> {
+    fn get_arg_or_file(arg: Option<String>, path: Option<String>) -> String {
         match arg {
-            Some(arg) => Ok(String::from(arg)),
+            Some(arg) => return arg,
             None => match path {
-                Some(path) => read_file(path),
-                None => Err(CliError::from(format!("No {} was supplied", name))),
+                Some(path) => return read_file(path.as_str()).unwrap(),
+                None => return String::new(),
             },
         }
     }
 
-    fn parse_pump_limit(pump_limit_arg: Option<&str>) -> i64 {
-        return pump_limit_arg
-            .and_then(|pump_limit| -> Option<i64> {
-                return Some(parse_size(pump_limit).unwrap_or(1024 ^ 2));
-            })
-            .unwrap_or(1024 ^ 2);
-    }
-
-    fn escape_pattern(&self, pattern: String) {
-        let escaped = regex::escape(pattern.as_str());
+    fn escape_pattern(&self) {
+        let escaped = regex::escape(self.parsed_opts.pattern.as_str());
         print!("{}", escaped);
     }
 
-    fn process_text(
-        &self,
-        pattern: &str,
-        replacement: &str,
-        text: String,
-    ) -> Result<String, CliError> {
+    fn process_text(&self, text: String) -> Result<String, CliError> {
         return Ok(String::from(
-            Regex::new(pattern)?
-                .replace_all(text.as_str(), replacement)
+            Regex::new(self.parsed_opts.pattern.as_str())?
+                .replace_all(text.as_str(), self.parsed_opts.replacement.as_str())
                 .as_ref(),
         ));
     }
 
-    fn process_file(&self, pattern: &str, replacement: &str, path: &str) -> Result<(), CliError> {
+    fn process_file(&self, path: &str) -> Result<(), CliError> {
         debug!("Processing: {} => ", path);
         return read_file(path).and_then(|text| -> Result<(), CliError> {
-            let result = self.process_text(pattern, replacement, text);
+            let result = self.process_text(text);
             match result {
                 Ok(result) => {
                     debugln!("replaced");
-                    match self.inplace {
+                    match self.opts.inplace {
                         true => return write_file(path, result),
                         false => {
                             print!("{}", result);
@@ -220,14 +211,9 @@ impl Cli {
         });
     }
 
-    fn process_files(
-        &self,
-        pattern: &str,
-        replacement: &str,
-        files: Vec<String>,
-    ) -> Result<(), CliError> {
-        for path in files {
-            match self.process_file(pattern, replacement, path.as_str()) {
+    fn process_files(&self) -> Result<(), CliError> {
+        for path in self.opts.files.iter() {
+            match self.process_file(path.as_str()) {
                 Ok(_) => (),
                 Err(error) => errorln!("{}", error),
             }
@@ -235,12 +221,12 @@ impl Cli {
         return Ok(());
     }
 
-    fn process_stdin(&self, pattern: &str, replacement: &str) -> Result<(), CliError> {
+    fn process_stdin(&self) -> Result<(), CliError> {
         debugln!("Reading stdin");
         let mut handle = std::io::stdin();
         let mut buffer = ScanBuffer::new(
-            self.pump_limit as usize,
-            (self.pump_limit / 2) as usize,
+            self.parsed_opts.pump_limit as usize,
+            (self.parsed_opts.pump_limit / 2) as usize,
             '\0',
         );
 
@@ -248,7 +234,7 @@ impl Cli {
             let text = buffer.process(|b: &Vec<char>| {
                 return String::from_iter(b.iter());
             });
-            let result = self.process_text(pattern, replacement, text);
+            let result = self.process_text(text);
             match result {
                 Ok(result) => print!("{}", result),
                 Err(_) => (),
@@ -257,26 +243,18 @@ impl Cli {
         return Ok(());
     }
 
-    fn process_pattern(&self, pattern: &str, replacement: &str) -> Result<(), CliError> {
-        match self.files.clone() {
-            Some(files) => return self.process_files(pattern, replacement, files),
-            None => return self.process_stdin(pattern, replacement),
+    fn process_pattern(&self) -> Result<(), CliError> {
+        match self.opts.files.len() > 0 {
+            true => return self.process_files(),
+            false => return self.process_stdin(),
         }
     }
 
     fn run(&self) -> Result<(), CliError> {
-        set_debug(self.verbose);
-        match self.pattern.clone() {
-            Ok(pattern) => match self.escape {
-                true => return Ok(self.escape_pattern(pattern)),
-                false => match self.replacement.clone() {
-                    Ok(replacement) => {
-                        return self.process_pattern(pattern.as_str(), replacement.as_str())
-                    }
-                    Err(error) => return Err(error),
-                },
-            },
-            Err(error) => return Err(error),
+        set_debug(self.opts.verbose);
+        match self.opts.escape {
+            true => return Ok(self.escape_pattern()),
+            false => return self.process_pattern(),
         }
     }
 }
